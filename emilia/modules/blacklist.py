86a651f1396bdab6b020abd1b6202b681878e72a
Ayra Hikari <AyraHikari@gmail.com>
2019-07-05 13:01:32 +0700

    Settings: just update parser for blacklist

--- emilia/modules/blacklist.py
+++ emilia/modules/blacklist.py
@@ -348,7 +348,7 @@ def __migrate__(old_chat_id, new_chat_id):
 
 def __chat_settings__(chat_id, user_id):
 	blacklisted = sql.num_blacklist_chat_filters(chat_id)
-	return tl(user_id, "Ada {} kata daftar hitam.").format(blacklisted)
+	return tl(user_id, "Ada `{}` kata daftar hitam.").format(blacklisted)
 
 
 def __stats__():
