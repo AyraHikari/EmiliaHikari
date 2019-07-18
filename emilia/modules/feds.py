eaaafa31689474d4607e3208c9e360f33e1f3315
Ayra Hikari <AyraHikari@linuxmail.org>
2019-07-18 11:06:45 +0700

    Fixed feds stats and added one missing langs

--- emilia/modules/feds.py
+++ emilia/modules/feds.py
@@ -1689,7 +1689,7 @@ def welcome_fed(bot, update):
 def __stats__():
 	all_fbanned = sql.get_all_fban_users_global()
 	all_feds = sql.get_all_feds_users_global()
-	return tl(OWNER_ID).format(len(all_fbanned), len(all_feds))
+	return tl(OWNER_ID, "{} pengguna di fbanned, pada {} federasi").format(len(all_fbanned), len(all_feds))
 
 
 def __user_info__(user_id, chat_id):
