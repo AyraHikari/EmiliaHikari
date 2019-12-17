9dc6e24ea034ef4e80660c2c9c74c1a4a1c434b9
Ayra Hikari <ayrahikari@linuxmail.org>
2019-12-17 07:28:13 +0700

    Added cleaner for blue text [2/2]

---
+++ emilia/modules/helper_funcs/alternate.py
@@ -0,0 +1,20 @@
+import sys
+import traceback
+
+from functools import wraps
+from typing import Optional
+
+from telegram import User, Chat, ChatMember, Update, Bot
+from telegram import error
+
+from emilia import DEL_CMDS, SUDO_USERS, WHITELIST_USERS
+
+from emilia.modules import languages
+
+
+def send_message(message, text,  *args,**kwargs):
+	try:
+		return message.reply_text(text, *args,**kwargs)
+	except error.BadRequest as err:
+		if str(err) == "Reply message not found":
+			return message.reply_text(text, quote=False, *args,**kwargs)
